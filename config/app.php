3cd4648dc11ac142667bc51beed4142345ef4dce
vagrant <vagrant@homestead>
2019-03-27 08:03:23 +0000

    用户头像上传

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://laravel-bbs.rch.com/'),
+    'url' => env('APP_URL', 'http://laravel-bbs.rch.com'),
 
     /*
     |--------------------------------------------------------------------------
