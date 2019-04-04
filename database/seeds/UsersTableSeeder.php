377dd0a7841921f9d9e5a3683f01d070a586af5f
vagrant <vagrant@homestead>
2019-04-04 05:32:36 +0000

    laravel-permission权限扩展

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -48,5 +48,12 @@ class UsersTableSeeder extends Seeder
         $user->avatar = 'http://laravel-bbs.rch.com/uploadfile/images/avatars/20190327/1_1553675932_QKDmvfm0DlCxcMc.jpg';
         $user->save();
 
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
     }
 }
